1d6bc700c40b9e3fa8097e7e22f5ecd2980a2422
Amy Ru <amru@calpoly.edu>
2021-04-12 11:32:29 -0700

    Create Lab_5EC.ino

---
+++ Labs/Lab_5EC.ino
@@ -0,0 +1,83 @@
+// EE 322 - Prof Chuck Bland
+// Amy Ru
+// Lab 5 EC 8 LED
+
+int ledRed1 = 13;
+int ledRed2 = 12;
+int ledYellow1 = 11;
+int ledYellow2 = 10;
+int ledGreen1 = 9;
+int ledGreen2 = 8;
+int ledBlue1 = 7;
+int ledBlue2 = 6;
+
+void setup() {
+  pinMode(ledRed1, OUTPUT);
+  pinMode(ledRed2, OUTPUT);
+  pinMode(ledYellow1, OUTPUT);
+  pinMode(ledYellow2, OUTPUT);
+  pinMode(ledGreen1, OUTPUT);
+  pinMode(ledGreen2, OUTPUT);
+  pinMode(ledBlue1, OUTPUT);
+  pinMode(ledBlue2, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledRed1, HIGH);
+  delay(100);
+  digitalWrite(ledRed1, LOW);
+  delay(100);
+  digitalWrite(ledRed2, HIGH);
+  delay(100);
+  digitalWrite(ledRed2, LOW);
+  delay(100);
+  digitalWrite(ledYellow1, HIGH);
+  delay(100);
+  digitalWrite(ledYellow1, LOW);
+  delay(100);
+  digitalWrite(ledYellow2, HIGH);
+  delay(100);
+  digitalWrite(ledYellow2, LOW);
+  delay(100);
+  digitalWrite(ledGreen1, HIGH);
+  delay(100);
+  digitalWrite(ledGreen1, LOW);
+  delay(100);
+  digitalWrite(ledGreen2, HIGH);
+  delay(100);
+  digitalWrite(ledGreen2, LOW);
+  delay(100);
+  digitalWrite(ledBlue1, HIGH);
+  delay(100);
+  digitalWrite(ledBlue1, LOW);
+  delay(100);
+  digitalWrite(ledBlue2, HIGH);
+  delay(100);
+  digitalWrite(ledBlue2, LOW);
+  delay(100);
+  digitalWrite(ledBlue1, HIGH);
+  delay(100);
+  digitalWrite(ledBlue1, LOW);
+  delay(100);
+  digitalWrite(ledGreen2, HIGH);
+  delay(100);
+  digitalWrite(ledGreen2, LOW);
+  delay(100);
+  digitalWrite(ledGreen1, HIGH);
+  delay(100);
+  digitalWrite(ledGreen1, LOW);
+  delay(100);
+  digitalWrite(ledYellow2, HIGH);
+  delay(100);
+  digitalWrite(ledYellow2, LOW);
+  delay(100);
+  digitalWrite(ledYellow1, HIGH);
+  delay(100);
+  digitalWrite(ledYellow1, LOW);
+  delay(100);
+  digitalWrite(ledRed2, HIGH);
+  delay(100);
+  digitalWrite(ledRed2, LOW);
+  delay(100);
+  
+}
