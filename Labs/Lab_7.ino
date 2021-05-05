f7024654497ebc0889964d6c8bc4371458f029a8
Amy Ru <amru@calpoly.edu>
2021-05-04 21:26:54 -0700

    clean up code

--- Labs/Lab_7.ino
+++ Labs/Lab_7.ino
@@ -2,96 +2,79 @@
 // Amy Ru
 // EE 322 - Prof Chuck Bland
 
-#define trigPin 7
-#define echoPin 6
-#define led 13
-#define led2 12
-#define led3 11
-#define led4 10
-#define led5 9
-#define led6 8
-#define buzzer 3
 
-int sound = 250;
+// constants need to be in UPPERCASE
+// use "courier" font for better readiability and clean code!!
+
+#include <HCSR04.h>
+
+// initialize trig pins and leds
+const int TRIGGERPIN = 13;
+const int ECHOPIN = 12;
+const int REDLED = 8;
+const int GREENLED = 6; 
+const int BLUELED = 5;
+const int BUZZER = 3;
+
+UltraSonicDistanceSensor distanceSensor(TRIGGERPIN, ECHOPIN);
+// this is a type, not a variable name
+
+void setup () 
+    {
+    Serial.begin(9600); // initialize baud rate at 9600
+    pinMode(REDLED, OUTPUT); 
+    pinMode(GREENLED, OUTPUT);
+    pinMode(BLUELED, OUTPUT);
+    pinMode(BUZZER, OUTPUT);
+    }
+
+void loop () 
+    {
+    // Every 500 miliseconds, do a measurement using the sensor and print the distance in centimeters
+    double distance = distanceSensor.measureDistanceCm();
+
+      if (distance == -1) // if out of range, no lights
+        {
+        digitalWrite(REDLED, LOW);
+        digitalWrite(GREENLED, LOW);
+        digitalWrite(BLUELED, LOW);
+        }
+        
+      else if (distance <= 75) // if distance is less than 75 cm, red light on
+        {
+        digitalWrite(REDLED, HIGH);
+        digitalWrite(GREENLED, LOW);
+        digitalWrite(BLUELED, LOW);
+
+        makeNoise(distance);
+        }
+        
+      else if (distance > 75 && distance < 150) // if distance is between 75 and 150 cm, green light on
+        {
+        digitalWrite(REDLED, LOW);
+        digitalWrite(GREENLED, HIGH);
+        digitalWrite(BLUELED, LOW);
+
+        makeNoise(distance);
+        }
+        
+      else // all other scenarios (> 150 cm), blue light on
+        {
+        digitalWrite(REDLED, LOW);
+        digitalWrite(GREENLED, LOW);
+        digitalWrite(BLUELED, HIGH);
+
+        makeNoise(distance);
+        }
+
+      Serial.print(distance);
+    }
 
 
-void setup() {
-  Serial.begin (9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(led, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
-  pinMode(led5, OUTPUT);
-  pinMode(led6, OUTPUT);
-  pinMode(buzzer, OUTPUT);
-
-}
-
-void loop() {
-  long duration, distance;
-  digitalWrite(trigPin, LOW); 
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration/2) / 29.1;
-
-
-  if (distance <= 30) {
-    digitalWrite(led, HIGH);
-    sound = 250;
-}
-  else {
-    digitalWrite(led,LOW);
+void makeNoise(double timeDelay) // cast variable timeDelay as a double
+  {
+  digitalWrite(BUZZER, HIGH); // sound ON
+  delay((int)timeDelay);
+  digitalWrite(BUZZER, LOW); // sound OFF
+  delay((int)timeDelay);
   }
-  if (distance < 25) {
-      digitalWrite(led2, HIGH);
-      sound = 260;
-}
-  else {
-      digitalWrite(led2, LOW);
-  }
-  if (distance < 20) {
-      digitalWrite(led3, HIGH);
-      sound = 270;
-} 
-  else {
-    digitalWrite(led3, LOW);
-  }
-  if (distance < 15) {
-    digitalWrite(led4, HIGH);
-    sound = 280;
-}
-  else {
-    digitalWrite(led4,LOW);
-  }
-  if (distance < 10) {
-    digitalWrite(led5, HIGH);
-    sound = 290;
-}
-  else {
-    digitalWrite(led5,LOW);
-  }
-  if (distance < 5) {
-    digitalWrite(led6, HIGH);
-    sound = 300;
-}
-  else {
-    digitalWrite(led6,LOW);
-  }
-
-  if (distance > 30 || distance <= 0){
-    Serial.println("Out of range");
-    noTone(buzzer);
-  }
-  else {
-    Serial.print(distance);
-    Serial.println(" cm");
-    tone(buzzer, sound);
-
-  }
-  delay(500);
-}
